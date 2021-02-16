ec36ce16d7671445ce42104ac2d5d30a41beb4a3
Carlos Cabrera Semidey <ccarlosalejandros@gmail.com>
2021-02-16 14:36:20 -0500

    First Setup, adminUser and feed added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { ChatEngine } from "react-chat-engine";
+import "./App.css";
 
 function App() {
+
+  const PROJECT_ID = "f421f74b-09aa-4fde-986e-c23f7afd0ec0";
+  const USERNAME = "CaleDev";
+  const USERSECRET = "2253";
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChatEngine
+      height="100vh"
+      projectID= {PROJECT_ID}
+      userName= {USERNAME}
+      userSecret= {USERSECRET}
+    />
   );
 }
 
